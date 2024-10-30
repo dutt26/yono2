702660121e29d6a777f7da5ecc599fa20bbcc884
dutt26 <gsyashwanthdutt26@gmail.com>
2024-10-30 16:36:14 +0000

    3rdcommit-dep203

--- head.java
+++ head.java
@@ -21,3 +21,18 @@ class Main {
     System.out.println("Reversed Number: " + reversed);
   }
 }
+
+//Reverse a number using a for loop in Java
+class Main {
+  public static void main(String[] args) {
+    
+    int num = 1234567, reversed = 0;
+
+    for(;num != 0; num /= 10) {
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+  }
+}
